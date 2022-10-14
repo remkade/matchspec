82cb941fd26f839477733a1c0f606185bd51101c
Kyle Leaders <kleaders@anaconda.com>
2022-10-13 21:07:54 -0700

    More parsing benchmarks

--- benches/parsing.rs
+++ benches/parsing.rs
@@ -2,6 +2,20 @@ use criterion::{black_box, criterion_group, criterion_main, Criterion};
 use matchspec::MatchSpec;
 
 fn criterion_benchmark(c: &mut Criterion) {
+    c.bench_function("Package name only", |b| {
+        b.iter(|| {
+            // This is a complex but not unlikely matchspec
+            black_box("tzdata")
+                .parse::<MatchSpec<String>>()
+        })
+    });
+    c.bench_function("Package name and version", |b| {
+        b.iter(|| {
+            // This is a complex but not unlikely matchspec
+            black_box("openssl>1.1.1g")
+                .parse::<MatchSpec<String>>()
+        })
+    });
     c.bench_function("All possible matchers", |b| {
         b.iter(|| {
             // This is a complex but not unlikely matchspec
