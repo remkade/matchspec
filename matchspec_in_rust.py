9c4cf459150f2459a2dd6ba4847493208feb1b45
Marco Esters <mesters@anaconda.com>
2022-11-10 16:24:13 -0800

    Change build tool to maturin

--- matchspec_in_rust.py
+++
@@ -1,4 +0,0 @@
-import rust_matchspec
-
-def match_against_matchspec(matchspec: str, package: str, version: str) -> bool:
-    return rust_matchspec.match_against_matchspec(matchspec, package, version)
