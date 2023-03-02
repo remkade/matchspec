1d875ca6022b34d199790b78c4d2e7362af15aaa
Askat Asanaliev <aasanaliev@anaconda.com>
2023-03-02 12:31:42 -0600

    move python API to dedicated module

--- src/lib.rs
+++ src/lib.rs
@@ -5,26 +5,6 @@ mod input_table;
 mod parsers;
 pub mod package_candidate;
 
-pub use crate::matchspec::*;
-
 #[cfg(feature = "python")]
-use pyo3::prelude::*;
-#[cfg(feature = "python")]
-use pyo3::wrap_pyfunction;
+pub mod python;
 
-
-/// This function matches matchspec string against package name and version
-#[cfg(feature = "python")]
-#[pyfunction]
-#[pyo3(signature = (matchspec, package, version))]
-fn match_against_matchspec(matchspec: String, package: String, version: String) -> bool {
-    let ms: matchspec::MatchSpec<String> = matchspec.parse().unwrap();
-    ms.is_package_version_match(&package, &version).unwrap()
-}
-
-#[cfg(feature = "python")]
-#[pymodule]
-fn rust_matchspec(_py: Python, m: &PyModule) -> PyResult<()> {
-    m.add_function(wrap_pyfunction!(match_against_matchspec, m)?)?;
-    Ok(())
-}
