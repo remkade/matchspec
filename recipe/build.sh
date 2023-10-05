65f67948612b41bbc0af96b72a0a6887afa0ed91
Serhii Kupriienko <skupriienko@anaconda.com>
2023-10-05 14:03:31 +0300

    Imporove & update recipe, add more data to pyproject.toml

--- recipe/build.sh
+++ recipe/build.sh
@@ -9,4 +9,4 @@ export RUSTUP_HOME=${CARGO_HOME}/rustup
 
 maturin build --release --strip --manylinux off --interpreter="${PYTHON}"
 
-"${PYTHON}" -m pip install $SRC_DIR/target/wheels/${PKG_NAME}*-cp${PY_VER/\./}-cp${PY_VER/\./}*.whl --no-deps -vv
+"${PYTHON}" -m pip install $SRC_DIR/target/wheels/${PKG_NAME}*-cp${PY_VER/\./}-cp${PY_VER/\./}*.whl --no-deps --no-build-isolation -vv
